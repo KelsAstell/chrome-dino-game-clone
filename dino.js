25e07777fa96838d8d86a7befd4197e195167ea8
大以巴狼艾斯 <435570123@qq.com>
2023-06-25 16:17:18 +0800

    Update dino.js

--- dino.js
+++ dino.js
@@ -7,8 +7,8 @@ import {
 const dinoElem = document.querySelector("[data-dino]")
 const JUMP_SPEED = 0.45
 const GRAVITY = 0.0015
-const DINO_FRAME_COUNT = 2
-const FRAME_TIME = 100
+const DINO_FRAME_COUNT = 3
+const FRAME_TIME = 250
 
 let isJumping
 let dinoFrame
